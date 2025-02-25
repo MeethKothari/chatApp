4f4f93c7ba5a6d39bb6ae61a4957e9ebb1ddb70c
Meeth <meethukothari@gmail.com>
2025-02-25 09:30:55 +0530

    added render link

--- frontend/ipconfig.js
+++ frontend/ipconfig.js
@@ -1,3 +1,3 @@
-const apiUrl = 'http://localhost:8082'; // local api server
-
+//const apiUrl = 'http://localhost:8082'; // local api server
+const apiUrl = `https://chatapp-aalw.onrender.com/` // render link
 export default apiUrl;
